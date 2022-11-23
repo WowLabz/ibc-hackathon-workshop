320dc405c9873b5601c32c76651572f64a971144
Loaki07 <66478092+Loaki07@users.noreply.github.com>
2022-11-23 21:59:47 +0000

    add: pallet_kitties template

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -47,6 +47,7 @@ pub use sp_runtime::{Perbill, Permill};
 
 /// Import the template pallet.
 pub use pallet_template;
+pub use pallet_kitties;
 
 /// An index to a block.
 pub type BlockNumber = u32;
@@ -278,6 +279,10 @@ impl pallet_template::Config for Runtime {
 	type RuntimeEvent = RuntimeEvent;
 }
 
+impl pallet_kitties::Config for Runtime {
+	type RuntimeEvent = RuntimeEvent;
+}
+
 // Create the runtime by composing the FRAME pallets that were previously configured.
 construct_runtime!(
 	pub struct Runtime
@@ -296,6 +301,7 @@ construct_runtime!(
 		Sudo: pallet_sudo,
 		// Include the custom logic from the pallet-template in the runtime.
 		TemplateModule: pallet_template,
+		Kitties: pallet_kitties,
 	}
 );
 
